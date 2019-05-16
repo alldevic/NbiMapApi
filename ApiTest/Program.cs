db1b3bf284abb1527810f41dcb75657111a13c89
Nikolay Bely <belyynikolay@gmail.com>
2019-05-16 16:55:50 +0700

    Remove unneeded packages

--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
+using RestSharp;
 
 namespace ApiTest
 {
     internal class Program
     {
+        
+
         public static void Main(string[] args)
         {
+            var client = new RestClient();
             var jsonData = LoadJson();
-            foreach (var item in jsonData)
-            {
+//            foreach (var item in jsonData)
+//            {
+            var item = jsonData.First();       
                 Console.WriteLine($"'{item.Name}': {item.Address}");
-            }
+                var request =
+                    new RestRequest(
+                        $"https://geocode-maps.yandex.ru/1.x/?format=json&apikey=0972a63d-5f64-4dfb-9020-8669bb2ad1ae&geocode={item.Address}",
+                        Method.GET, DataFormat.Json);
+                var response = client.Get(request);
+                Console.WriteLine(response.Content);
+//            }
         }
 
         public static List<Item> LoadJson()
--- NbiMapApi.jsClient/Codebehind/app.js
+++ NbiMapApi.jsClient/Codebehind/app.js
@@ -1,12 +1,19 @@
-angular.module('MyApp', ['ymaps']).controller('MapCtrl', function($scope) {
-    //создаем массив координат и данных для меток и балунов
-    $scope.markers = [
-        {coordinates:[56.56, 38.63], properties: {balloonContent: 'Здесь рыбы нет!'}},
-        {coordinates:[55.16, 39.89], properties: {balloonContent: 'Здесь рыбы тоже нет'}, options: {preset: 'islands#icon', iconColor: '#a5260a'}},
-        {coordinates:[55.08, 38.96], properties: {balloonContent: 'А здесь есть!'}}
-    ];
-    //настройки положения карты
-    $scope.map = {
-        center: [55.55, 39.84], zoom: 12
-    };
-});
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [35.76, 37.64],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+
+    myMap.geoObjects
+        .add(new ymaps.Placemark([87.13139, 53.756254], {
+            balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+        }, {
+            preset: 'islands#icon',
+            iconColor: '#0095b6'
+        }));
+}
