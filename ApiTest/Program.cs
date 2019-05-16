ca714efcc455519ac25404f355ea72f69a3b3c5d
Nikolay Bely <belyynikolay@gmail.com>
2019-05-16 13:51:23 +0700

    Add json file loading

--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace ApiTest
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ApiTest
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var jsonData = LoadJson();
+            foreach (var item in jsonData)
+            {
+                Console.WriteLine($"'{item.Name}': {item.Address}");
+            }
+        }
+
+        public static List<Item> LoadJson()
+        {
+            using (var r = new StreamReader("data.json"))
+            {
+                return JsonConvert.DeserializeObject<List<Item>>(r.ReadToEnd());
+            }
+        }
+
+        public class Item
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
         }
     }
 }
